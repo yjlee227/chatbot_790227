8bfd85298f34d11f09bf1cb52e7fde7d4aa6e3d7
yjlee227 <mikael227@gmail.com>
2025-09-15 20:37:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,10 +42,35 @@ else:
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
             model="gpt-3.5-turbo",
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
+            # 대화 메시지 리스트를 생성합니다.
+            # 각 메시지는 'role'과 'content'로 이루어져 있습니다.
+            messages = [
+              {
+                # 시스템 메시지는 모델의 기본 역할이나 성격을 정의합니다.
+                # 모델에게 똑똑하고 창의적이라는 역할을 부여합니다.
+                "role": "system",
+                "content": 
+                """
+                당신은 테슬라 자동차의 매니저이며, 모든 메뉴얼에 대해서 설명할 수 있다.
+                그리고, 자율주행 관련 최신뉴스를 제공할 수 있다. 
+                추측성 답변은 할 수 없으며, 항상 확인된 정보를 제공하며 그 설명은 한글로만 제공하는 챗봇이다.  
+                """
+              },
+              {
+                "role": "assistant",
+                "content":
+                "답변은 항상 자연수를 사용하여 순번 1부터 시작한다."
+                "두번째 답변은 순번 2로 시작한다.."
+                "준비한 답변이 끝날때까지 순번을 사용한다."  
+                },
+              {
+                # 'user'는 사용자가 모델에게 보내는 메시지를 나타냅니다.
+                # 사용자가 '안녕하세요!'라고 인사합니다.
+                    "role": "user",
+                "content": 
+                  "오토파일럿에 대해서 알려줘"
+              }
+            ],            
             stream=True,
         )
 
